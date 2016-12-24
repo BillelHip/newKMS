4f18de4260dc60a82b48d77dfe7b896f15587887
BilHip <barbaoui@gmail.com>
2016-12-25 02:47:12 +0800

    KMS

---
+++ pages/prototyping.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title> Prototyping </title>
+    <style>
+        .wrap {
+            width: 100%;
+            height: 100%;
+            overflow:auo;
+        }
+
+        .fleft {
+            float:left;
+            width: 50%;
+            height: 100%;
+        }
+
+        .fright {
+            float: right;
+            height: 100%;
+            width: 50%;
+        }
+    </style>
+</head>
+
+<body>
+
+<div class="wrap">
+    <div class="fleft">
+
+        <iframe src="index.html" width="700" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+
+
+    </div>
+    <div class="fright">
+
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSex8O8do0vYsQx9KhK3yqXkdm8tY4NMuSr8H0UyF3rO97bIeA/viewform?embedded=true" width="700" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+
+    </div>
+
+</div>
+<div style="clear:both"></div>
+</body>
+
+</html>
